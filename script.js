6cd429493a19f551c909c7328df57df033165ffd
Júlia Pereira <julia.pereira.p31@gmail.com>
2025-09-03 14:09:42 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Botão funcional
+document.getElementById("btn-download").addEventListener("click", () => {
+  // coloque aqui o link que você quiser
+  window.location.href = "https://play.google.com/";
+});
